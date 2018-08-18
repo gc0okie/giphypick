62dbf254ed9ad166bfc7941958758f4d0a6434cf
kent <kentyamada@gmail.com>
2018-08-18 12:07:43 -0400

    test6

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 const giphy = require('giphy-api')('hQZZ5e4DdsdQqY4prZpckQetnyZZpbqP');
 const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.use(express.json());
 
